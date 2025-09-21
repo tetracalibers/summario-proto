f9201a476196f5afb697908d83b4f19c608717d5
tomixy <92695929+tetracalibers@users.noreply.github.com>
2025-09-22 06:18:44 +0900

    Heading Buttonの設置

--- app/editor/Actionbar.tsx
+++ app/editor/Actionbar.tsx
@@ -1,5 +1,7 @@
 import type { Editor } from "@tiptap/react"
+import { ButtonGroup } from "~/components/tiptap-ui-primitive/button"
 import { CodeBlockButton } from "~/components/tiptap-ui/code-block-button"
+import { HeadingButton } from "~/components/tiptap-ui/heading-button"
 
 interface Props {
   editor: Editor
@@ -8,7 +10,12 @@ interface Props {
 const EditorActionbar = ({ editor }: Props) => {
   return (
     <>
-      <CodeBlockButton editor={editor} hideWhenUnavailable={true} showShortcut={true} />
+      <ButtonGroup orientation="horizontal">
+        <HeadingButton editor={editor} level={2} hideWhenUnavailable={true} showShortcut={true} />
+        <HeadingButton editor={editor} level={3} hideWhenUnavailable={true} showShortcut={true} />
+        <HeadingButton editor={editor} level={4} hideWhenUnavailable={true} showShortcut={true} />
+        <CodeBlockButton editor={editor} hideWhenUnavailable={true} showShortcut={true} />
+      </ButtonGroup>
     </>
   )
 }
