a9c169bdfacc6d0043fa7650e123793acba88285
tomixy <92695929+tetracalibers@users.noreply.github.com>
2025-09-22 06:27:34 +0900

    List Buttonの配置

--- app/editor/Actionbar.tsx
+++ app/editor/Actionbar.tsx
@@ -2,6 +2,7 @@ import type { Editor } from "@tiptap/react"
 import { ButtonGroup } from "~/components/tiptap-ui-primitive/button"
 import { CodeBlockButton } from "~/components/tiptap-ui/code-block-button"
 import { HeadingButton } from "~/components/tiptap-ui/heading-button"
+import { ListButton } from "~/components/tiptap-ui/list-button"
 
 interface Props {
   editor: Editor
@@ -14,6 +15,24 @@ const EditorActionbar = ({ editor }: Props) => {
         <HeadingButton editor={editor} level={2} hideWhenUnavailable={true} showShortcut={true} />
         <HeadingButton editor={editor} level={3} hideWhenUnavailable={true} showShortcut={true} />
         <HeadingButton editor={editor} level={4} hideWhenUnavailable={true} showShortcut={true} />
+        <ListButton
+          editor={editor}
+          type="bulletList"
+          hideWhenUnavailable={true}
+          showShortcut={true}
+        />
+        <ListButton
+          editor={editor}
+          type="orderedList"
+          hideWhenUnavailable={true}
+          showShortcut={true}
+        />
+        <ListButton
+          editor={editor}
+          type="taskList"
+          hideWhenUnavailable={true}
+          showShortcut={true}
+        />
         <CodeBlockButton editor={editor} hideWhenUnavailable={true} showShortcut={true} />
       </ButtonGroup>
     </>
