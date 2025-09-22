f30bbce08e65a8776f99fd86e103feafbd29533a
tomixy <92695929+tetracalibers@users.noreply.github.com>
2025-09-22 16:04:46 +0900

    Toggle Sectionでセクション内のすべての要素に対してセクション囲み解除を行う

--- app/editor/Actionbar.tsx
+++ app/editor/Actionbar.tsx
@@ -78,7 +78,31 @@ const EditorActionbar = ({ editor }: Props) => {
         />
         <button
           type="button"
-          onClick={() => editor.chain().focus().toggleWrap("sectionBlock").run()}
+          onClick={() => {
+            const $sectionBlocks = editor.$nodes("sectionBlock")
+            if (!$sectionBlocks) {
+              editor.chain().focus().toggleWrap("sectionBlock").run()
+              return
+            }
+
+            const activeNodePos = editor.state.selection.$head
+            const activeSectionBlock = $sectionBlocks.find((node) => {
+              return (
+                activeNodePos.pos >= node.pos && activeNodePos.pos <= node.pos + node.node.nodeSize
+              )
+            })
+            if (!activeSectionBlock) {
+              editor.chain().focus().toggleWrap("sectionBlock").run()
+              return
+            }
+
+            const from = activeSectionBlock.pos + 1
+            const to = activeSectionBlock.pos + activeSectionBlock.node.nodeSize - 1
+
+            for (let i = from; i <= to; i++) {
+              editor.chain().focus(i).toggleWrap("sectionBlock").run()
+            }
+          }}
         >
           Toggle Section
         </button>
