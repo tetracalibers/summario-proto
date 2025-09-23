5e0459a115afdc3f63fe9a87ef32051f6231ac3f
tomixy <92695929+tetracalibers@users.noreply.github.com>
2025-09-23 10:22:26 +0900

    toolbarコンポーネントによるデザイン調整

--- app/editor/Actionbar.tsx
+++ app/editor/Actionbar.tsx
@@ -1,9 +1,10 @@
 import type { Editor } from "@tiptap/react"
-import { ButtonGroup } from "~/components/tiptap-ui-primitive/button"
 import { CodeBlockButton } from "~/components/tiptap-ui/code-block-button"
 import { HeadingButton } from "~/components/tiptap-ui/heading-button"
 import { LinkPopover } from "~/components/tiptap-ui/link-popover"
 import { ListButton } from "~/components/tiptap-ui/list-button"
+import { Toolbar } from "~/components/tiptap-ui-primitive/toolbar"
+import { Button } from "~/components/tiptap-ui-primitive/button"
 
 interface Props {
   editor: Editor
@@ -11,103 +12,101 @@ interface Props {
 
 const EditorActionbar = ({ editor }: Props) => {
   return (
-    <>
-      <ButtonGroup orientation="horizontal">
-        <LinkPopover editor={editor} hideWhenUnavailable={false} autoOpenOnLinkActive={true} />
-        <HeadingButton
-          editor={editor}
-          level={2}
-          hideWhenUnavailable={false}
-          showShortcut={true}
-          onClick={() => {
-            const chain = editor.chain().focus()
-            if (editor.isActive("heading", { level: 2 })) {
-              chain.setParagraph().run()
-            } else {
-              chain.setHeading({ level: 2 }).run()
-            }
-          }}
-        />
-        <HeadingButton
-          editor={editor}
-          level={3}
-          hideWhenUnavailable={false}
-          showShortcut={true}
-          onClick={() => {
-            const chain = editor.chain().focus()
-            if (editor.isActive("heading", { level: 3 })) {
-              chain.setParagraph().run()
-            } else {
-              chain.setHeading({ level: 3 }).run()
-            }
-          }}
-        />
-        <HeadingButton
-          editor={editor}
-          level={4}
-          hideWhenUnavailable={false}
-          showShortcut={true}
-          onClick={() => {
-            const chain = editor.chain().focus()
-            if (editor.isActive("heading", { level: 4 })) {
-              chain.setParagraph().run()
-            } else {
-              chain.setHeading({ level: 4 }).run()
-            }
-          }}
-        />
-        <ListButton
-          editor={editor}
-          type="bulletList"
-          hideWhenUnavailable={false}
-          showShortcut={true}
-          onClick={() => editor.chain().focus().toggleBulletList().run()}
-        />
-        <ListButton
-          editor={editor}
-          type="orderedList"
-          hideWhenUnavailable={false}
-          showShortcut={true}
-          onClick={() => editor.chain().focus().toggleOrderedList().run()}
-        />
-        <CodeBlockButton
-          editor={editor}
-          hideWhenUnavailable={false}
-          showShortcut={true}
-          onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-        />
-        <button
-          type="button"
-          onClick={() => {
-            const $sectionBlocks = editor.$nodes("sectionBlock")
-            if (!$sectionBlocks) {
-              editor.chain().focus().toggleWrap("sectionBlock").run()
-              return
-            }
+    <Toolbar>
+      <LinkPopover editor={editor} hideWhenUnavailable={false} autoOpenOnLinkActive={true} />
+      <HeadingButton
+        editor={editor}
+        level={2}
+        hideWhenUnavailable={false}
+        showShortcut={true}
+        onClick={() => {
+          const chain = editor.chain().focus()
+          if (editor.isActive("heading", { level: 2 })) {
+            chain.setParagraph().run()
+          } else {
+            chain.setHeading({ level: 2 }).run()
+          }
+        }}
+      />
+      <HeadingButton
+        editor={editor}
+        level={3}
+        hideWhenUnavailable={false}
+        showShortcut={true}
+        onClick={() => {
+          const chain = editor.chain().focus()
+          if (editor.isActive("heading", { level: 3 })) {
+            chain.setParagraph().run()
+          } else {
+            chain.setHeading({ level: 3 }).run()
+          }
+        }}
+      />
+      <HeadingButton
+        editor={editor}
+        level={4}
+        hideWhenUnavailable={false}
+        showShortcut={true}
+        onClick={() => {
+          const chain = editor.chain().focus()
+          if (editor.isActive("heading", { level: 4 })) {
+            chain.setParagraph().run()
+          } else {
+            chain.setHeading({ level: 4 }).run()
+          }
+        }}
+      />
+      <ListButton
+        editor={editor}
+        type="bulletList"
+        hideWhenUnavailable={false}
+        showShortcut={true}
+        onClick={() => editor.chain().focus().toggleBulletList().run()}
+      />
+      <ListButton
+        editor={editor}
+        type="orderedList"
+        hideWhenUnavailable={false}
+        showShortcut={true}
+        onClick={() => editor.chain().focus().toggleOrderedList().run()}
+      />
+      <CodeBlockButton
+        editor={editor}
+        hideWhenUnavailable={false}
+        showShortcut={true}
+        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+      />
+      <Button
+        type="button"
+        onClick={() => {
+          const $sectionBlocks = editor.$nodes("sectionBlock")
+          if (!$sectionBlocks) {
+            editor.chain().focus().toggleWrap("sectionBlock").run()
+            return
+          }
 
-            const activeNodePos = editor.state.selection.$head
-            const activeSectionBlock = $sectionBlocks.find((node) => {
-              return (
-                activeNodePos.pos >= node.pos && activeNodePos.pos <= node.pos + node.node.nodeSize
-              )
-            })
-            if (!activeSectionBlock) {
-              editor.chain().focus().toggleWrap("sectionBlock").run()
-              return
-            }
+          const activeNodePos = editor.state.selection.$head
+          const activeSectionBlock = $sectionBlocks.find((node) => {
+            return (
+              activeNodePos.pos >= node.pos && activeNodePos.pos <= node.pos + node.node.nodeSize
+            )
+          })
+          if (!activeSectionBlock) {
+            editor.chain().focus().toggleWrap("sectionBlock").run()
+            return
+          }
 
-            const from = activeSectionBlock.pos + 1
-            const to = activeSectionBlock.pos + activeSectionBlock.node.nodeSize - 1
+          const from = activeSectionBlock.pos + 1
+          const to = activeSectionBlock.pos + activeSectionBlock.node.nodeSize - 1
 
-            for (let i = from; i <= to; i++) {
-              editor.chain().focus(i).toggleWrap("sectionBlock").run()
-            }
-          }}
-        >
-          Toggle Section
-        </button>
-      </ButtonGroup>
-    </>
+          for (let i = from; i <= to; i++) {
+            editor.chain().focus(i).toggleWrap("sectionBlock").run()
+          }
+        }}
+      >
+        Toggle Group
+      </Button>
+    </Toolbar>
   )
 }
 
