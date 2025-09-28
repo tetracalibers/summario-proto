c1142cdee27bd51ebd26b97e9958a810e9448ae4
tomixy <92695929+tetracalibers@users.noreply.github.com>
2025-09-28 23:23:12 +0900

    UI配置の仮決め

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -9,17 +9,25 @@ export default function Index() {
   return (
     <TiptapProvider>
       <div className={styles.root}>
-        <header className={styles.header}>Header</header>
+        <header className={styles.header}>
+          <pre>Node Title</pre>
+          <pre>Alias Input</pre>
+          <pre>Save</pre>
+          <pre>Delete Note</pre>
+        </header>
         <main className={styles.main}>
           <TipTapEditor />
         </main>
         <div className={styles.side_l}>
+          <pre>Search Input</pre>
           <FolderTree data={folderTreeDummyData} />
+          <pre>Network Graph</pre>
         </div>
         <div className={styles.side_r}>
+          <pre>Related Terms Input</pre>
+          <pre>References Input</pre>
           <BlockTypeMenu />
         </div>
-        <footer className={styles.footer}>Footer</footer>
       </div>
     </TiptapProvider>
   )
