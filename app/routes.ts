5cce95276af6fa5672357dc684a8a35905982f22
tomixy <92695929+tetracalibers@users.noreply.github.com>
2025-09-30 18:00:24 +0900

    Editorのテストページ削除

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,5 @@ import { type RouteConfig, index, layout, route } from "@react-router/dev/routes
 
 export default [
   index("routes/index.tsx"),
-  layout("layouts/editor-page-layout.tsx", [route("terms/:termId", "routes/term.tsx")]),
-  route("editor", "routes/home.tsx")
+  layout("layouts/editor-page-layout.tsx", [route("terms/:termId", "routes/term.tsx")])
 ] satisfies RouteConfig
