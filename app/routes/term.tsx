a1b8500ad6072ccf6aea786418b4f7a783e07b48
tomixy <92695929+tetracalibers@users.noreply.github.com>
2025-09-30 17:25:15 +0900

    実際の関連用語データを基にNetworkGraphを表示

--- app/routes/term.tsx
+++ app/routes/term.tsx
@@ -1,20 +1,25 @@
-import { Button } from "@mantine/core"
+import { Accordion, Button, TagsInput } from "@mantine/core"
 import SaveButton from "~/components/term-note/SaveButton"
 import { getTermById } from "~/service/term"
 import EditorWith from "~/components/editor/EditorWith"
 import type { Route } from "./+types/term"
-import SideRight from "~/layouts/note/SideRight"
+import { findRelatedTerms } from "~/service/related-term"
+import NetworkGraph from "~/components/network-graph/NetworkGraph"
 
 export async function loader({ params }: Route.LoaderArgs) {
   const { termId } = params
-  const term = await getTermById(termId)
+  const [term, graphData] = await Promise.all([
+    getTermById(termId),
+    findRelatedTerms(Number(termId))
+  ])
   if (!term) throw new Response("No term found", { status: 404 })
 
-  return { term }
+  return { term, graphData }
 }
 
-export default function Term({ loaderData }: Route.ComponentProps) {
-  const { term } = loaderData
+export default function Term({ loaderData, params }: Route.ComponentProps) {
+  const { term, graphData } = loaderData
+  const { termId } = params
 
   return (
     <>
@@ -30,7 +35,23 @@ export default function Term({ loaderData }: Route.ComponentProps) {
         </EditorWith>
       </div>
       <div className="rightside-area">
-        <SideRight />
+        <Accordion variant="contained" multiple defaultValue={["alias"]}>
+          <Accordion.Item value="alias">
+            <Accordion.Control>エイリアスの設定</Accordion.Control>
+            <Accordion.Panel>
+              <TagsInput
+                label="Press Enter to submit a alias"
+                placeholder="Type and press enter..."
+              />
+            </Accordion.Panel>
+          </Accordion.Item>
+          <Accordion.Item value="related">
+            <Accordion.Control>関連用語の設定</Accordion.Control>
+            <Accordion.Panel>Related Input</Accordion.Panel>
+          </Accordion.Item>
+        </Accordion>
+        <NetworkGraph {...graphData} centerId={Number(termId)} />
+        <div>TODO: MiniView</div>
       </div>
     </>
   )
