3f8a11a8659c016f807e6d72e40fb6c6df7f1c19
tomixy <92695929+tetracalibers@users.noreply.github.com>
2025-10-09 06:07:54 +0900

    dagreによる自動レイアウトを試す

--- app/routes/folder-map.tsx
+++ app/routes/folder-map.tsx
@@ -5,25 +5,89 @@ import {
   useNodesState,
   useEdgesState,
   addEdge,
-  type Connection
+  type Connection,
+  Position,
+  ConnectionLineType
 } from "@xyflow/react"
 import "@xyflow/react/dist/style.css"
 import { useCallback } from "react"
+import dagre from "@dagrejs/dagre"
 
+const dagreGraph = new dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}))
+
+const nodeWidth = 172
+const nodeHeight = 36
+
+const getLayoutedElements = (nodes: Node[], edges: Edge[], direction = "LR") => {
+  const isHorizontal = direction === "LR"
+  dagreGraph.setGraph({ rankdir: direction })
+
+  nodes.forEach((node) => {
+    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight })
+  })
+
+  edges.forEach((edge) => {
+    dagreGraph.setEdge(edge.source, edge.target)
+  })
+
+  dagre.layout(dagreGraph)
+
+  const newNodes = nodes.map((node) => {
+    const nodeWithPosition = dagreGraph.node(node.id)
+    const newNode = {
+      ...node,
+      targetPosition: isHorizontal ? Position.Left : Position.Top,
+      sourcePosition: isHorizontal ? Position.Right : Position.Bottom,
+      // We are shifting the dagre node position (anchor=center center) to the top left
+      // so it matches the React Flow node anchor point (top left).
+      position: {
+        x: nodeWithPosition.x - nodeWidth / 2,
+        y: nodeWithPosition.y - nodeHeight / 2
+      }
+    }
+
+    return newNode
+  })
+
+  return { nodes: newNodes, edges }
+}
+
+const position = { x: 0, y: 0 }
 const initialNodes: Node[] = [
-  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
-  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } }
+  { id: "1", position, data: { label: "1" }, type: "input" },
+  { id: "2", position, data: { label: "2" }, type: "output" }
 ]
-
 const initialEdges: Edge[] = [{ id: "e1-2", source: "1", target: "2" }]
 
+const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
+  initialNodes,
+  initialEdges
+)
+
 export default function FolderMap() {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes)
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges)
+  const [nodes, setNodes, onNodesChange] = useNodesState(layoutedNodes)
+  const [edges, setEdges, onEdgesChange] = useEdgesState(layoutedEdges)
 
   const onConnect = useCallback(
-    (params: Connection) => setEdges((eds: Edge[]) => addEdge(params, eds)),
+    (params: Connection) =>
+      setEdges((eds) =>
+        addEdge({ ...params, type: ConnectionLineType.SmoothStep, animated: true }, eds)
+      ),
     [setEdges]
+  )
+
+  const onLayout = useCallback(
+    (direction: string) => {
+      const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
+        nodes,
+        edges,
+        direction
+      )
+
+      setNodes([...layoutedNodes])
+      setEdges([...layoutedEdges])
+    },
+    [nodes, edges]
   )
 
   return (
@@ -34,6 +98,7 @@ export default function FolderMap() {
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        connectionLineType={ConnectionLineType.SmoothStep}
         fitView
         attributionPosition="bottom-left"
       />
