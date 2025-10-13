b471947d3defa045833a85a3d417cea2dd27318f
tomixy <92695929+tetracalibers@users.noreply.github.com>
2025-10-14 07:15:20 +0900

    ファイルクリック時にエディタのコンテンツが更新されない問題に対処

--- app/components/folder-explorer/FileLink.tsx
+++ app/components/folder-explorer/FileLink.tsx
@@ -13,7 +13,7 @@ interface Props {
 
 export default function FileLink({ targetTerm }: Props) {
   return (
-    <NavLink to={`/terms/${targetTerm.id}`} className={styles.link}>
+    <NavLink to={`/terms/${targetTerm.id}`} className={styles.link} reloadDocument>
       <ItemContent
         label={targetTerm.name}
         Icon={() => <IconNote size={18} color="var(--mantine-color-cyan-4)" />}
