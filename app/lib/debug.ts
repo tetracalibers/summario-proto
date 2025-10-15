367362b58ad3f8ad55b790ad3ba420a490e15a8a
tomixy <92695929+tetracalibers@users.noreply.github.com>
2025-10-16 06:14:22 +0900

    debugLogで出力される関数名が`debugLog`になってしまう問題を修正

--- app/lib/debug.ts
+++ app/lib/debug.ts
@@ -3,8 +3,8 @@ import { getCallSites } from "node:util"
 export const debugLog = (...args: any[]) => {
   if (!import.meta.env.DEV) return
 
-  // 呼び出し元フレームを1件だけ取得（ソースマップ対応も可）
-  const [site] = getCallSites(1, { sourceMap: true })
+  // debugLog() の呼び出し元を取得
+  const [_, site] = getCallSites()
   const fn = site?.functionName || "anonymous"
 
   console.log(`[${fn}]`, ...args)
