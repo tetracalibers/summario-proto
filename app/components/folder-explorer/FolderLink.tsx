3a88c7b7ce5b897386f2caff6554e648fbf9aa94
tomixy <92695929+tetracalibers@users.noreply.github.com>
2025-10-15 13:40:40 +0900

    フォルダを右クリックでコンテキストメニューを表示

--- app/components/folder-explorer/FolderLink.tsx
+++ app/components/folder-explorer/FolderLink.tsx
@@ -1,7 +1,8 @@
-import { IconFolderFilled } from "@tabler/icons-react"
-import { UnstyledButton } from "@mantine/core"
+import { IconFolderFilled, IconTrash, IconEdit } from "@tabler/icons-react"
+import { Menu, UnstyledButton } from "@mantine/core"
 import { clsx } from "clsx"
 import styles from "./EntryLink.module.css"
+import { useState } from "react"
 
 interface Props {
   folderName: string
@@ -10,17 +11,45 @@ interface Props {
 }
 
 export default function FolderLink({ folderName, isActiveStyle, onClick }: Props) {
+  const [openedMenu, setOpenedMenu] = useState(false)
+
   return (
-    <UnstyledButton
-      onClick={onClick}
-      className={clsx(
-        styles.entry_link,
-        styles.folder_link,
-        isActiveStyle && styles.highlight_active
-      )}
+    <Menu
+      shadow="md"
+      offset={0}
+      width={200}
+      position="right-start"
+      withArrow
+      arrowPosition="center"
+      opened={openedMenu}
+      onChange={setOpenedMenu}
     >
-      <IconFolderFilled size={18} />
-      <span className={styles.label}>{folderName}</span>
-    </UnstyledButton>
+      <Menu.Target aria-label="folder action menu">
+        <UnstyledButton
+          onClick={onClick}
+          onContextMenu={(e) => {
+            e.preventDefault()
+            setOpenedMenu(true)
+          }}
+          className={clsx(
+            styles.entry_link,
+            styles.folder_link,
+            isActiveStyle && styles.highlight_active,
+            openedMenu && styles.highlight_focused
+          )}
+        >
+          <IconFolderFilled size={18} />
+          <span className={styles.label}>{folderName}</span>
+        </UnstyledButton>
+      </Menu.Target>
+
+      <Menu.Dropdown>
+        <Menu.Item leftSection={<IconEdit size={14} />}>Rename</Menu.Item>
+        <Menu.Divider />
+        <Menu.Item color="red" leftSection={<IconTrash size={14} />}>
+          Delete
+        </Menu.Item>
+      </Menu.Dropdown>
+    </Menu>
   )
 }
