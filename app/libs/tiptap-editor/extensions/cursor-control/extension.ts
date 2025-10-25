c88deed0027c1051e9d27e0bdd34ecf3407dd7f6
tomixy <92695929+tetracalibers@users.noreply.github.com>
2025-10-25 13:58:00 +0900

    setCursorToPrevNodeEndコマンドのバグ修正

--- app/libs/tiptap-editor/extensions/cursor-control/extension.ts
+++ app/libs/tiptap-editor/extensions/cursor-control/extension.ts
@@ -25,10 +25,15 @@ export const CursorControl = Extension.create({
           if (pos < 1) return true // ドキュメントの最初のノードでは何もしない
           const prevPos = state.doc.resolve(pos - 1)
 
-          const isLastNode =
-            state.doc.resolve(state.doc.content.size).nodeBefore === $from.node($from.depth - 1)
-          if (isLastNode) {
+          const isLastNode = $from.parentOffset === $from.parent.nodeSize - 2 // ノード末尾にいるかどうか
+          const isCaretAtEnd = $from.pos === $from.end()
+
+          if (isLastNode && isCaretAtEnd) {
             return chain().setTextSelection(TextSelection.atEnd(state.doc)).run()
+          }
+
+          if (isCaretAtEnd) {
+            return chain().setTextSelection(TextSelection.atEnd(prevPos.node())).run()
           }
 
           // setTextSelectionコマンドを使用するとカーソルの位置を設定できる
