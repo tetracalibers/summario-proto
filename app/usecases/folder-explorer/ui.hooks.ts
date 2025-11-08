5695d31fcb8ef728b43162062896813191e088bf
tomixy <92695929+tetracalibers@users.noreply.github.com>
2025-11-09 07:23:14 +0900

    別フォルダに遷移しない限り現在のfolderIdがセットされない問題を修正

--- app/usecases/folder-explorer/ui.hooks.ts
+++ app/usecases/folder-explorer/ui.hooks.ts
@@ -2,8 +2,11 @@ import { useAtom } from "jotai"
 import { folderId$ } from "./ui.atoms"
 import { useQuery } from "@tanstack/react-query"
 import type { loader } from "~/routes/api/folders/children"
+import { useSyncAtom } from "~/libs/jotai-utils/hooks"
 
 export const useFolderExplorerUi = (initials: Awaited<ReturnType<typeof loader>>) => {
+  useSyncAtom(folderId$, initials.current?.id ?? null)
+
   const [folderId, setFolderId] = useAtom(folderId$)
 
   const { data, isPending, isError } = useQuery<Awaited<ReturnType<typeof loader>>>({
